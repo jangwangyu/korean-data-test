1c9fc704afabee674428a374fb9d396e2887ce3b
dkfdj <dkfdj01033@gmail.com>
2025-03-06 02:35:16 +0900

    #5 - 스프링 부트 프로젝트 초기화
    인텔리제이에서 Spring Initialzr를 통해 프로젝트 초기화, 당장 필요할 것으로 예상되는 디펜던시 추가

---
+++ src/main/java/org/example/koreandatatest/KoreanDataTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.koreandatatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanDataTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreanDataTestApplication.class, args);
+	}
+
+}
