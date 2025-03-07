342affe933d3852cd6ae420aea49bf0ea844165e
dkfdj <dkfdj01033@gmail.com>
2025-03-07 21:14:44 +0900

    #9 - MockData: MockDataType에 최근 enum 변경을 반영
    직전 작업 #8에서 'mockDataType' 필드는
    enum으로 관리하게 되었음\
    이 변경을 'MockData'에 반영

--- src/main/java/org/example/koreandatatest/domain/MockData.java
+++ src/main/java/org/example/koreandatatest/domain/MockData.java
@@ -3,13 +3,14 @@ package org.example.koreandatatest.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.example.koreandatatest.domain.constant.MockDataType;
 
 @Getter
 @Setter
 @ToString
 public class MockData {
 
-  private String mockDataType;
+  private MockDataType mockDataType;
   private String mockDataValue;
 
 
