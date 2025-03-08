e842ba5c544c88d3d65215d72e90cb2a134d7e22
dkfdj <dkfdj01033@gmail.com>
2025-03-08 22:43:57 +0900

    #11 - 도메인 클래스를 엔티티로 전환
    각 도메인이 실제로 JPA를 통해
    db에 접근할 수 있도록
    엔티티 연관 정보를 추가함.

--- src/main/java/org/example/koreandatatest/domain/SchemaField.java
+++ src/main/java/org/example/koreandatatest/domain/SchemaField.java
@@ -1,20 +1,86 @@
 package org.example.koreandatatest.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.example.koreandatatest.domain.constant.MockDataType;
 
-@Getter
-@Setter
-@ToString
-public class SchemaField {
+/**
+ * 특정 {@link TableSchema}의 단위 필드 정보. 이 필드들이 모여서 테이블 스키마를 구성한다.
+ *
+ * @author dkfdj
+ */
 
-  private String fieldName;
-  private MockDataType mockDataType; // enum을 다룰 수 있게끔
-  private Integer fieldOrder;
-  private Integer blackPercent;
+@Getter
+@Entity
+@ToString(callSuper = true)
+public class SchemaField extends AuditingFields{
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Setter
+  @ManyToOne(optional = false)
+  private TableSchema tableSchema;
+
+  @Setter private @Column(nullable = false) MockDataType mockDataType; // enum을 다룰 수 있게끔
+  @Setter private @Column(nullable = false) Integer fieldOrder;
+  @Setter private @Column(nullable = false) Integer blankPercent;
+  @Setter private @Column(nullable = false) String fieldName;
+
   private String typeOptionJson; // JSON 형태로 저장 {min: 1, max: 10, ...}
   private String forceValue; // 강제로 넣을 값
 
+  protected SchemaField() {
+  }
+
+  private SchemaField(String fieldName, MockDataType mockDataType, Integer fieldOrder,
+      Integer blankPercent, String typeOptionJson, String forceValue) {
+    this.fieldName = fieldName;
+    this.mockDataType = mockDataType;
+    this.fieldOrder = fieldOrder;
+    this.blankPercent = blankPercent;
+    this.typeOptionJson = typeOptionJson;
+    this.forceValue = forceValue;
+  }
+
+  public static SchemaField of(String fieldName, MockDataType mockDataType, Integer fieldOrder,
+      Integer blankPercent, String typeOptionJson, String forceValue) {
+    return new SchemaField(fieldName, mockDataType, fieldOrder, blankPercent, typeOptionJson,
+        forceValue);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof SchemaField that)) {
+      return false;
+    }
+    if (that.getId() == null) {
+      return Objects.equals(this.getTableSchema().getId(), that.getTableSchema().getId()) &&
+              Objects.equals(this.getMockDataType(), that.getMockDataType()) &&
+              Objects.equals(this.getFieldName(), that.getFieldName()) &&
+              Objects.equals(this.getFieldOrder(), that.getFieldOrder()) &&
+              Objects.equals(this.getBlankPercent(), that.getBlankPercent()) &&
+              Objects.equals(this.getTypeOptionJson(), that.getTypeOptionJson()) &&
+              Objects.equals(this.getForceValue(), that.getForceValue());
+    }
+    return Objects.equals(this.getId(), that.getId());
+  }
+
+
+  @Override
+  public int hashCode() {
+    if(getId() == null) {
+      return Objects.hash(getTableSchema().getId(), getMockDataType(),getFieldName(),getFieldOrder(),getBlankPercent(),getTypeOptionJson(),getForceValue());
+    }
+    return Objects.hash(getId());
+  }
 }
--- src/main/java/org/example/koreandatatest/domain/TableSchema.java
+++ src/main/java/org/example/koreandatatest/domain/TableSchema.java
@@ -1,17 +1,104 @@
 package org.example.koreandatatest.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * 단위 테이블 스키마 정보.
+ * 식별자({@link #userId}) 로 특정할 수 있는 회원이 소유한다.
+ *
+ * @author dkfdj
+ */
+
 @Getter
-@Setter
-@ToString // toString() 메서드 자동 생성
-public class TableSchema {
+@ToString(callSuper = true) // toString() 메서드 자동 생성
+@Entity
+public class TableSchema extends AuditingFields{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  private String schemaName;
-  private String userId;
-  private LocalDateTime exportedAt;
+  @Setter private String schemaName;
+  @Setter private String userId;
+  @Setter private LocalDateTime exportedAt;
 
+  private LocalDateTime createdAt;
+  private String createdBy;
+  private LocalDateTime modifiedAt;
+  private String modifiedBy;
+
+  @ToString.Exclude
+  @OneToMany(mappedBy = "tableSchema", cascade = CascadeType.ALL, orphanRemoval = true)
+  private final Set<SchemaField> schemaFields = new LinkedHashSet<>();
+
+
+  protected TableSchema() {}
+
+  private TableSchema(String schemaName, String userId) {
+    this.schemaName = schemaName;
+    this.userId = userId;
+    this.exportedAt = null;
+  }
+
+  public static TableSchema of(String schemaName, String userId) {
+    return new TableSchema(schemaName, userId);
+  }
+
+  public void markExported() { // export된 시간을 기록
+    exportedAt = LocalDateTime.now();
+  }
+
+  public boolean isExported() { // export된 시간이 기록되어 있는지 확인
+    return exportedAt != null;
+  }
+
+  // SchemaField를 컬렉션 레벨에서 제어할 수 있느 기능 추가
+  public void addSchemaField(SchemaField schemaField) { // schemaField 추가
+    schemaField.setTableSchema(this);
+    schemaFields.add(schemaField);
+  }
+
+  public void addSchemaFields(Collection<SchemaField> schemaFields) { // schemaField 여러개 추가
+    schemaFields.forEach(this::addSchemaField);
+  }
+
+  public void clearSchemaFields() { // schemaField 모두 삭제
+    schemaFields.clear();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof TableSchema that)) {
+      return false;
+    }
+    if(that.getId() == null) {
+      return Objects.equals(getSchemaName(), that.getSchemaName()) &&
+              Objects.equals(getUserId(), that.getUserId()) &&
+              Objects.equals(getExportedAt(), that.getExportedAt()) &&
+              Objects.equals(getSchemaFields(), that.getSchemaFields());
+    }
+
+    return Objects.equals(getId(), that.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    if (getId() == null){
+      return Objects.hash(getSchemaName(), getUserId(), getExportedAt(), getSchemaFields());
+    }
+
+    return Objects.hash(getId());
+  }
 }
