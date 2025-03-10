b22fcde7eb45230a695de63c3313407ea93b0b29
dkfdj <dkfdj01033@gmail.com>
2025-03-11 00:50:20 +0900

    #19 - request dto 업데이트 : of()메소드 추가
    편의를 위해 생성자 메소드를 추가함

--- src/main/java/org/example/koreandatatest/DTO/request/SchemaFieldRequest.java
+++ src/main/java/org/example/koreandatatest/DTO/request/SchemaFieldRequest.java
@@ -11,6 +11,23 @@ public record SchemaFieldRequest(
     String typeOptionJson,
     String forceValue
 ) {
+  public static SchemaFieldRequest of(
+      String fieldName,
+      MockDataType mockDataType,
+      Integer fieldOrder,
+      Integer blankPercent,
+      String typeOptionJson,
+      String forceValue
+  ) {
+    return new SchemaFieldRequest(
+        fieldName,
+        mockDataType,
+        fieldOrder,
+        blankPercent,
+        typeOptionJson,
+        forceValue
+    );
+  }
 
   public SchemaFieldDto toDto() {
     return SchemaFieldDto.of(
--- src/main/java/org/example/koreandatatest/DTO/request/TableSchemaRequest.java
+++ src/main/java/org/example/koreandatatest/DTO/request/TableSchemaRequest.java
@@ -5,14 +5,21 @@ import java.util.stream.Collectors;
 import org.example.koreandatatest.DTO.TableSchemaDto;
 
 public record TableSchemaRequest(
-    String tableName,
+    String schemaName,
     String userId,
     List<SchemaFieldRequest> schemaFields
 ) {
+  public static TableSchemaRequest of(
+      String schemaName,
+      String userId,
+      List<SchemaFieldRequest> schemaFields
+  ) {
+    return new TableSchemaRequest(schemaName, userId, schemaFields);
+  }
 
   public TableSchemaDto toDto() {
     return TableSchemaDto.of(
-        tableName,
+        schemaName,
         userId,
         null,
         schemaFields.stream()
