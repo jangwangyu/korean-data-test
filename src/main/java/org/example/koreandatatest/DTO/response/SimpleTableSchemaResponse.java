538820fd9b6724590feea1c4c5291bb6218d604a
dkfdj <dkfdj01033@gmail.com>
2025-03-14 16:42:12 +0900

    #22 - 내 스키마 목록 dto에 수정일시 추가
    내 스키마 목록 화면에 수정일시 정보를 표시하기 위해
    데이터 스펙 추가

--- src/main/java/org/example/koreandatatest/DTO/response/SimpleTableSchemaResponse.java
+++ src/main/java/org/example/koreandatatest/DTO/response/SimpleTableSchemaResponse.java
@@ -1,14 +1,16 @@
 package org.example.koreandatatest.DTO.response;
 
+import java.time.LocalDateTime;
 import org.example.koreandatatest.DTO.TableSchemaDto;
 
 public record SimpleTableSchemaResponse(
     String schemaName,
-    String userId
+    String userId,
+    LocalDateTime modifiedAt
 ) {
 
   public static SimpleTableSchemaResponse fromDto(TableSchemaDto dto) {
-    return new SimpleTableSchemaResponse(dto.schemaName(), dto.userId());
+    return new SimpleTableSchemaResponse(dto.schemaName(), dto.userId(), dto.modifiedAt());
   }
 
 
