32b5fc0cf02734b84092994d8962a8cbf0b6a2f7
dkfdj <dkfdj01033@gmail.com>
2025-03-15 17:50:35 +0900

    #24 - OAuth2 기능 구현
    OAuth2 github로 인증 로그인을 할 수 있게 기능 구현을 함

--- src/main/java/org/example/koreandatatest/config/SecurityConfig.java
+++ src/main/java/org/example/koreandatatest/config/SecurityConfig.java
@@ -1,8 +1,17 @@
 package org.example.koreandatatest.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.example.koreandatatest.DTO.security.GithubUser;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // 스프링 설정을 위한 클래스임을 명시
@@ -11,9 +20,24 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // 모든 요청에 대해 접근을 허용
-            .logout(logout -> logout.logoutSuccessUrl("/")) // 로그아웃 성공 시 홈 페이지로 이동
-            .build();
+        .authorizeHttpRequests(auth ->
+            auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations()) .permitAll()// 정적 리소스에 대한 요청은 모두 허용
+                .requestMatchers(
+                    HttpMethod.GET,
+                    "/",
+                    "table-schema",
+                    "table-schema/export"
+                ).permitAll()
+                .anyRequest().authenticated())
+        .oauth2Login(withDefaults())
+        .logout(logout -> logout.logoutSuccessUrl("/")) // 로그아웃 성공 시 홈 페이지로 이동
+        .build();
   }
 
+  @Bean
+  public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+    final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+    return userRequest -> GithubUser.from(delegate.loadUser(userRequest).getAttributes());
+  }
 }
--- src/test/java/org/example/koreandatatest/controller/UserAccountControllerTest.java
+++ src/test/java/org/example/koreandatatest/controller/UserAccountControllerTest.java
@@ -1,11 +1,13 @@
 package org.example.koreandatatest.controller;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.example.koreandatatest.DTO.security.GithubUser;
 import org.example.koreandatatest.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @DisplayName("[Controller] 회원 컨트롤러 테스트")
@@ -21,18 +22,20 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest
 public record UserAccountControllerTest(@Autowired MockMvc mvc) {
 
-  @WithMockUser
   @DisplayName("[Get] 내 정보 페이지 -> 내 정보 뷰 (정상)")
   @Test
   void givenAuthenticatedUser_whenRequesting_thenShowsMyAccountView() throws Exception {
     // Given
-
+    var githubUser = new GithubUser("test-id", "test-name", "test@email.com");
     // When&Then
-    mvc.perform(get("/my-account"))
+    mvc.perform(
+        get("/my-account")
+        .with(oauth2Login().oauth2User(githubUser))
+        )
         .andExpect(status().isOk())
         .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-        .andExpect(model().attributeExists("nickname"))
-        .andExpect(model().attributeExists("email"))
+        .andExpect(model().attribute("nickname", githubUser.name()))
+        .andExpect(model().attribute("email", githubUser.email()))
         .andExpect(view().name("my-account"));
   }
 }
