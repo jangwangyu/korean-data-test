d562932ede07fb0582bfb630e27c2949ff0ed273
dkfdj <dkfdj01033@gmail.com>
2025-03-20 22:56:39 +0900

    #28 - Cache 구현
    select문이 50번이 반복 출력이 되어서 Cache로 한번만 출력시키게끔 구현

--- src/main/java/org/example/koreandatatest/repository/MockDataRepository.java
+++ src/main/java/org/example/koreandatatest/repository/MockDataRepository.java
@@ -1,8 +1,14 @@
 package org.example.koreandatatest.repository;
 
+import java.util.List;
 import org.example.koreandatatest.domain.MockData;
+import org.example.koreandatatest.domain.constant.MockDataType;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MockDataRepository extends JpaRepository<MockData, Long> {
+
+  @Cacheable("mockData")
+  List<MockData> findByMockDataType(MockDataType mockDataType);
 
 }
